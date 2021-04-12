3269d28f294dc364036f3595c5cd414025f2e387
kounetsuman <punkrou404@gmail.com>
2021-04-12 12:28:06 +0900

    TUTORIAL: Node.jsのモジュールについて

--- src/app.js
+++ src/app.js
@@ -8,3 +8,9 @@ app.get('/', function (req, res) {
 app.listen(8000, function () {
     console.log('Example app listening on port 8000!');
 });
+
+var square1 = require('./square1'); // Here we require() the name of the file without the (optional) .js file extension
+console.log('The area of a square1 with a width of 4 is ' + square1.area(4));
+
+var square2 = require('./square2'); // Here we require() the name of the file without the (optional) .js file extension
+console.log('The area of a square2 with a width of 4 is ' + square2.area(4));
